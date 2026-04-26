ebefc784438d76ba859cb1a2cb444236a21ed9c1
LecyLecy <adinza11806@gmail.com>
2026-04-27 01:39:41 +0700

    init: setup project structure and environment

---
+++ environment_test.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import sklearn
+import streamlit as st
+import joblib
+
+print("Environment is ready.")
+print("Pandas:", pd.__version__)
+print("NumPy:", np.__version__)
+print("Scikit-learn:", sklearn.__version__)
